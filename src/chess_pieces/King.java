841660906399ac902e81becc5fdcd01b8ced8eeb
PiotrG25 <piotrgoralewicz25@gmail.com>
2019-10-04 17:42:36 +0200

    King moves

--- src/chess_pieces/King.java
+++ src/chess_pieces/King.java
@@ -1,12 +1,29 @@
 package chess_pieces;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class King extends ChessPiece{
 
 
+    public King(int xPos, int yPos, boolean isWhite) {
+        super(xPos, yPos, isWhite);
+    }
+
 
     @Override
-    public int[][] getPossibleMoves(boolean[][] freeSpace) {
+    public List<int[]> getPossibleMoves(boolean[][] freeSpace, boolean[][] opponents) {
+        List<int []> moves = new ArrayList<>();
 
+        for(int i = xPos - 1; i <= xPos + 1; i++){
+            for(int j = yPos - 1; j <= yPos + 1; j++){
+                if((i != xPos || j != yPos) && 0 <= i && i < 8 && 0 <= j && j < 8 && (freeSpace[i][j] || opponents[i][j])){
+                    moves.add(new int []{xPos + i, yPos + j});
+                }
+            }
+        }
+
+        return moves;
     }
 
     @Override
