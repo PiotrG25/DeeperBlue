e54344529e48183baa71123e533576dda0e56a15
PiotrG25 <piotrgoralewicz25@gmail.com>
2019-10-04 14:36:36 +0200

    Rook moves

--- src/chess_pieces/Rook.java
+++ src/chess_pieces/Rook.java
@@ -1,12 +1,33 @@
 package chess_pieces;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Rook extends ChessPiece{
 
 
+    public Rook(int xPos, int yPos, boolean isWhite) {
+        super(xPos, yPos, isWhite);
+    }
+
 
     @Override
-    public int[][] getPossibleMoves(boolean[][] freeSpace) {
+    public List<int[]> getPossibleMoves(boolean[][] freeSpace, boolean[][] opponents) {
+        List<int []> moves = new ArrayList<>();
 
+        for(int i = xPos + 1; i < 8 && (freeSpace[i][yPos] || opponents[i][yPos]); i++){
+            moves.add(new int []{i, yPos});
+        }
+        for(int i = xPos - 1; i >= 0 && (freeSpace[i][yPos] || opponents[i][yPos]); i--){
+            moves.add(new int []{i, yPos});
+        }
+        for(int j = yPos + 1; j < 8 && (freeSpace[xPos][j] || opponents[xPos][j]); j++){
+            moves.add(new int []{xPos, j});
+        }
+        for(int j = yPos - 1; j >= 0 && (freeSpace[xPos][j] || opponents[xPos][j]); j--){
+            moves.add(new int []{xPos, j});
+        }
+        return moves;
     }
 
     @Override
