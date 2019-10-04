b116e0c93593dbf7337fd180145d5f72036c51bf
PiotrG25 <piotrgoralewicz25@gmail.com>
2019-10-04 14:38:24 +0200

    Queen moves

--- src/chess_pieces/Queen.java
+++ src/chess_pieces/Queen.java
@@ -1,12 +1,49 @@
 package chess_pieces;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Queen extends ChessPiece{
 
 
+    public Queen(int xPos, int yPos, boolean isWhite) {
+        super(xPos, yPos, isWhite);
+    }
+
 
     @Override
-    public int[][] getPossibleMoves(boolean[][] freeSpace) {
+    public List<int[]> getPossibleMoves(boolean[][] freeSpace, boolean[][] opponents) {
+        List<int []> moves = new ArrayList<>();
 
+        // Rook like
+        for(int i = xPos + 1; i < 8 && (freeSpace[i][yPos] || opponents[i][yPos]); i++){
+            moves.add(new int []{i, yPos});
+        }
+        for(int i = xPos - 1; i >= 0 && (freeSpace[i][yPos] || opponents[i][yPos]); i--){
+            moves.add(new int []{i, yPos});
+        }
+        for(int j = yPos + 1; j < 8 && (freeSpace[xPos][j] || opponents[xPos][j]); j++){
+            moves.add(new int []{xPos, j});
+        }
+        for(int j = yPos - 1; j >= 0 && (freeSpace[xPos][j] || opponents[xPos][j]); j--){
+            moves.add(new int []{xPos, j});
+        }
+
+        // Bishop like
+        for(int i = xPos + 1, j = yPos + 1; i < 8 && j < 8 && (freeSpace[i][j] || opponents[i][j]); i++, j++){
+            moves.add(new int []{i, j});
+        }
+        for(int i = xPos + 1, j = yPos - 1; i < 8 && j >= 0 && (freeSpace[i][j] || opponents[i][j]); i++, j--){
+            moves.add(new int []{i, j});
+        }
+        for(int i = xPos - 1, j = yPos + 1; i >= 0 && j < 8 && (freeSpace[i][j] || opponents[i][j]); i--, j++){
+            moves.add(new int []{i, j});
+        }
+        for(int i = xPos - 1, j = yPos - 1; i >= 0 && j >= 0 && (freeSpace[i][j] || opponents[i][j]); i--, j--){
+            moves.add(new int []{i, j});
+        }
+
+        return moves;
     }
 
     @Override
